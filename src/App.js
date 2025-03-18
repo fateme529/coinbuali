ead3aece92b423468ef473b11a964ada20594bc4
fateme529 <fdamavandi529@gmail.com>
2025-03-19 00:43:57 +0330

    v 0.0.8

--- src/App.js
+++ src/App.js
@@ -1,21 +1,35 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import Loader from "./Loader"; // بارگذاری کامپوننت Loader
 import './App.css'; // فایل CSS شما
 import frameImage from './Frame2.png'; // وارد کردن تصویر
 
+
+
+
 function App() {
   const [isLoading, setIsLoading] = useState(true); // وضعیت بارگذاری
-  const [isWelcomeVisible, setIsWelcomeVisible] = useState(false); // وضعیت نمایش پیام خوش آمد گویی
+  const [isWelcomeVisible, setIsWelcomeVisible] = useState(true); // نمایش پیام خوش آمد گویی
+  const [isContentVisible, setIsContentVisible] = useState(false); // نمایش محتوای اصلی
 
   // تنظیم زمان لودینگ
   useEffect(() => {
     const timer = setTimeout(() => {
       setIsLoading(false); // بعد از ۳ ثانیه لودینگ تمام می‌شود
-      setIsWelcomeVisible(true); // نمایش پیام خوش آمد گویی بعد از لودینگ
-    }, 3000); // ۳ ثانیه
+    }, 3000);
 
     return () => clearTimeout(timer); // پاک کردن تایمر هنگام تغییر وضعیت
   }, []);
+
+  useEffect(() => {
+    if (!isLoading) {
+      const welcomeTimer = setTimeout(() => {
+        setIsWelcomeVisible(false); // بعد از ۲ ثانیه پیام خوش آمد گویی مخفی می‌شود
+        setIsContentVisible(true); // محتوای اصلی نمایش داده می‌شود
+      }, 2000);
+
+      return () => clearTimeout(welcomeTimer); // پاک کردن تایمر پیام خوش آمد گویی
+    }
+  }, [isLoading]);
 
   if (isLoading) {
     return <Loader />; // در حین بارگذاری کامپوننت Loader را نمایش بده
@@ -24,16 +38,13 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        {isWelcomeVisible && (
-          <>
-            <h1>Welcome to BualiCoin!</h1>
-            {/* نمایش تصویر بعد از لودینگ */}
-            <img
-              src={frameImage} // استفاده از تصویر وارد شده
-              alt="Coin"
-              className="w-32 h-32 animate-pulse"
-            />
-          </>
+        {isWelcomeVisible && <h1>Welcome to BualiCoin!</h1>} 
+        {isContentVisible && (
+          <div>
+            {}
+            
+            <img src={frameImage}  alt="Your Content" />
+          </div>
         )}
       </header>
     </div>

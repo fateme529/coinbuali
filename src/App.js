a986eef00fac9d51980d22a967b458321f62cb2a
fateme529 <fdamavandi529@gmail.com>
2025-03-19 18:23:01 +0330

    v 0.0.2

--- src/App.js
+++ src/App.js
@@ -1,33 +1,34 @@
 import { useState, useEffect } from "react";
-import Loader from "./Loader"; // بارگذاری کامپوننت Loader
+import Loader from "./Loader"; 
 import './App.css'; 
 import frameImage from './Frame2.png'; 
+import frameSigup from './MacBook Air - 2.png'; 
 
 
 
 
 function App() {
-  const [isLoading, setIsLoading] = useState(true); // وضعیت بارگذاری
+  const [isLoading, setIsLoading] = useState(true); 
   const [isWelcomeVisible, setIsWelcomeVisible] = useState(true); 
-  const [isContentVisible, setIsContentVisible] = useState(false); // نمایش محتوای اصلی
-
-  // تنظیم زمان لودینگ
+  const [isContentVisible, setIsContentVisible] = useState(false); 
+  const [isRegisterVisible, setIsRegisterVisible] = useState(false); 
+ 
   useEffect(() => {
     const timer = setTimeout(() => {
-      setIsLoading(false); // بعد از ۳ ثانیه لودینگ تمام می‌شود
+      setIsLoading(false);
     }, 3000);
 
-    return () => clearTimeout(timer); // پاک کردن تایمر هنگام تغییر وضعیت
+    return () => clearTimeout(timer);
   }, []);
 
   useEffect(() => {
     if (!isLoading) {
       const welcomeTimer = setTimeout(() => {
-        setIsWelcomeVisible(false); // بعد از ۲ ثانیه پیام خوش آمد گویی مخفی می‌شود
-        setIsContentVisible(true); // محتوای اصلی نمایش داده می‌شود
+        setIsWelcomeVisible(false); 
+        setIsContentVisible(true); 
       }, 2000);
 
-      return () => clearTimeout(welcomeTimer); // پاک کردن تایمر پیام خوش آمد گویی
+      return () => clearTimeout(welcomeTimer); 
     }
   }, [isLoading]);
 
@@ -39,11 +40,22 @@ function App() {
     <div className="App">
       <header className="App-header">
         {isWelcomeVisible && <h1>Welcome to BualiCoin!</h1>} 
-        {isContentVisible && (
+        {isContentVisible &&  !isRegisterVisible && (
           <div className="image-container">
-            {}
-            
+
             <img src={frameImage}  alt="Your Content" />
+            <button className="signup-button"onClick={() => setIsRegisterVisible(true)}>ثبت نام</button>
+          </div>
+        )}
+     
+        {isRegisterVisible && (
+          <div className="register-container">
+            <h2>فرم ثبت نام</h2>
+            <form>
+              <input type="text" placeholder="نام کاربری" />
+              <input type="password" placeholder="رمز عبور" />
+              <button type="submit">ثبت نام</button>
+            </form>
           </div>
         )}
       </header>
